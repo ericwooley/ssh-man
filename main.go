1cab1306a13bc3424ce5841c408534fc2295cbd6
Eric Wooley <ericwooley@gmail.com>
2026-04-07 00:14:33 +0000

    feat: bootstrap Wails app and embed assets

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "ssh-man/cmd/app"
+import (
+	"embed"
+	"log"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+
+	"ssh-man/internal/app/bindings"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
 
 func main() {
-	app.Main()
+	app, err := bindings.NewAppBindings()
+	if err != nil {
+		log.Fatalf("bootstrap application: %v", err)
+	}
+
+	err = wails.Run(&options.App{
+		Title:  "SSH Man",
+		Width:  1280,
+		Height: 840,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		Bind: []interface{}{
+			app,
+		},
+	})
+	if err != nil {
+		log.Fatalf("run application: %v", err)
+	}
 }
