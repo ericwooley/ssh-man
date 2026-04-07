826fb9cb46ff4a90b368490f346ae40b228019bb
Eric Wooley <ericwooley@gmail.com>
2026-04-07 19:06:25 +0000

    feat(browser): add proxy bypass and resolver rules

--- internal/platform/browser/launch_darwin.go
+++ internal/platform/browser/launch_darwin.go
@@ -9,6 +9,14 @@ func launchDarwin(option BrowserOption, socksPort int) error {
 	if !option.SupportsProxyLaunch {
 		return fmt.Errorf("%s does not support proxy launch in this MVP", option.DisplayName)
 	}
-	cmd := exec.Command("open", "-na", option.LaunchReference, "--args", fmt.Sprintf("--proxy-server=socks5://127.0.0.1:%d", socksPort))
+	cmd := exec.Command(
+		"open",
+		"-na",
+		option.LaunchReference,
+		"--args",
+		fmt.Sprintf("--proxy-server=socks5://127.0.0.1:%d", socksPort),
+		"--proxy-bypass-list=<-loopback>",
+		"--host-resolver-rules=MAP * ~NOTFOUND , EXCLUDE 127.0.0.1",
+	)
 	return cmd.Start()
 }
--- internal/platform/browser/launch_linux.go
+++ internal/platform/browser/launch_linux.go
@@ -9,6 +9,11 @@ func launchLinux(option BrowserOption, socksPort int) error {
 	if !option.SupportsProxyLaunch {
 		return fmt.Errorf("%s does not support proxy launch in this MVP", option.DisplayName)
 	}
-	cmd := exec.Command(option.LaunchReference, fmt.Sprintf("--proxy-server=socks5://127.0.0.1:%d", socksPort))
+	cmd := exec.Command(
+		option.LaunchReference,
+		fmt.Sprintf("--proxy-server=socks5://127.0.0.1:%d", socksPort),
+		"--proxy-bypass-list=<-loopback>",
+		"--host-resolver-rules=MAP * ~NOTFOUND , EXCLUDE 127.0.0.1",
+	)
 	return cmd.Start()
 }
